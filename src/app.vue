adca7a044a5581630ac7894d5bb2c19ae5b3ea12
dhuff <dallinhuff@gmail.com>
2024-11-10 23:18:01 -0700

    feat: add seo meta to head

--- src/app.vue
+++ src/app.vue
@@ -1,9 +1,19 @@
 <script setup lang="ts">
+const title = "R-Blocks: The Pinnacle of ICF";
+const description = `R-Blocks is the pinnacle of Insulated Concrete Forms (ICF) manufacturing. Our stay-in-place reinforced concrete wall molds are exclusively made with polyurethane foam and precision methods, offering superior construction advantages, lifetime savings, and value. Our products are backed by 30 years of experience in creating the most cost-efficient permanent building solutions for commercial and residential projects.`;
+
 useHead({
   titleTemplate: (titleChunk) => {
-    return titleChunk ? `${titleChunk} - R-Blocks` : "R-Blocks: The Pinnacle of ICF";
+    return titleChunk ? `${titleChunk} - R-Blocks` : title;
   },
 });
+
+useSeoMeta({
+  title,
+  ogTitle: title,
+  description,
+  ogDescription: description,
+})
 </script>
 
 <template>
